4a3d277653e388afebf95072882ec8b3eeba98ea
郭润桂 <gavin2026@gmail.com>
2018-03-06 17:57:45 +0800

    隐藏新浪微博Flash安装提示 onload为嘛会执行2次？？？

---
+++ hideSinaFlashInstallTips/hideSinaFlashInstallTips.user.js
@@ -0,0 +1,38 @@
+// ==UserScript==
+// @name         HideSinaFlashInstallTips
+// @name:zh-CN   隐藏新浪微博Flash安装提示
+// @namespace    BlueFire
+// @version      0.1
+// @description  去掉新浪微博弹出的提示“我们的产品需要安装flashplayer 10或更高版本”
+// @author       Gavin
+// @include      http://blog.sina.com.cn/*
+// @run-at       document-start
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+	let timer = 0;
+	let clearAlert = () => {
+        try{
+            let alertDom = document.getElementsByClassName("CP_w");
+            if(alertDom){
+                for(let i=0; i < alertDom.length; i++){
+					if("none" != alertDom[i].style.display){
+						alertDom[i].style.display = "none";
+						alertDom[i].previousElementSibling.style.display = "none";
+						clearInterval(timer);
+					}
+				}
+            }
+        }catch(e){
+        }
+	};
+
+    window.onload = function(){
+		timer = setInterval(clearAlert, 3000);
+		setTimeout(()=>{
+			clearInterval(timer);
+		},60000);
+    };
+})();
